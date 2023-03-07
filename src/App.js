e30bf4330da64b057247276fb317baeb4bb4c4a9
Radhe5050 <radheayar5050@gmail.com>
2023-03-07 17:05:51 +0530

    next commit added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Users from "./components/Users"
 
 function App() {
   return (
+    <>
     <h1>Hello World! Welcome to the blog APP</h1>
+    <Users />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
