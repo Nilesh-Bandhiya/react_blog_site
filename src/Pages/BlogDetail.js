1770dcc004063e8926077eec468e56ef2ee2fbe0
Radhe5050 <radheayar5050@gmail.com>
2023-03-13 23:51:37 +0530

    blogdetails get in blogdetail page

--- src/Pages/BlogDetail.js
+++ src/Pages/BlogDetail.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const BlogDetail = () => {
+  const blogs = useSelector((state) => state.blog?.blogs);
 
-const params = useParams();
+  const params = useParams();
+
+  let currentBlog = blogs.find((blog) => {
+    return blog.id == params.blogId;
+  });
 
   return (
     <div
-        className="ag-theme-alpine"
-        style={{
-          backgroundColor: "aqua",
-          margin: " 0 auto",
-          boxSizing: "border-box",
-          height: "80vh",
-          width: "85vw",
-        }}
-      >
+      className="ag-theme-alpine"
+      style={{
+        backgroundColor: "aqua",
+        margin: " 0 auto",
+        boxSizing: "border-box",
+        height: "80vh",
+        width: "85vw",
+      }}
+    >
       <h2>{params.blogId}</h2>
+      <img src={currentBlog.image} alt={currentBlog.title} />
     </div>
-  )
-}
+  );
+};
 
-export default BlogDetail
+export default BlogDetail;
