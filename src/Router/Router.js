eab27f601c5a7e480d6057162ae77087f77be937
Radhe5050 <radheayar5050@gmail.com>
2023-03-15 17:10:01 +0530

    change little code structure

---
+++ src/Router/Router.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BlogDetail from "../Pages/BlogDetail"
+import Blogs from "../Pages/Blogs";
+import Login from "../Pages/Login";
+import SignUp from "../Pages/SingUp";
+import Users from "../Pages/Users";
+import MainLayout from "../components/MainLayout";
+import { ProtectedLoginRoute, ProtectedAdminRoute, ProtectedLogoutRoute, ProtectedUserRoute } from "../Auth/ProtectedRoutes";
+import About from "../Pages/About";
+import UserDetail from "../Pages/UserDetail";
+
+const Router = () => {
+
+    const router = createBrowserRouter([
+        {
+          path: "/",
+          element: <MainLayout />,
+          children: [
+            { index: true, element: <Blogs  /> },
+            { path: "myblogs", element: <Blogs  /> },
+            { path: ":blogId", element: <ProtectedLoginRoute><BlogDetail /></ProtectedLoginRoute> },
+            { path: "users", element: <ProtectedAdminRoute><Users /></ProtectedAdminRoute> },
+            { path: "users/:userId", element: <ProtectedUserRoute><UserDetail /></ProtectedUserRoute> },
+            { path: "about", element: <About /> },
+            { path: "signup", element: <ProtectedLogoutRoute><SignUp /></ProtectedLogoutRoute> },
+            { path: "signin", element: <ProtectedLogoutRoute><Login /></ProtectedLogoutRoute> },
+          ],
+        },
+      ]);
+      
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default Router
